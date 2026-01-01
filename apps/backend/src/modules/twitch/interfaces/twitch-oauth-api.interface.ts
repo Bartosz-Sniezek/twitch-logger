c3723c251172cd514f62dc9e13eb5a4216c0343f
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-01-01 17:04:35 +0100

    fix typo in twitchOAuthApiResponseSchema

--- apps/backend/src/modules/twitch/interfaces/twitch-oauth-api.interface.ts
+++ apps/backend/src/modules/twitch/interfaces/twitch-oauth-api.interface.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 export const twitchOAuthApiResponseSchema = z.object({
   access_token: z.string(),
   expires_in: z.number(),
-  token_typee: z.string(),
+  token_type: z.string(),
 });
 
 export type TwitchOAuthApiResponse = z.infer<
